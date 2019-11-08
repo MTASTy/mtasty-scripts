358200f3fdbbbf7fb918f6e5e49428fc71b7173b
Timur Popov <timur2915@gmail.com>
2019-11-08 22:03:57 +0300

    Fix types in generate-meta

--- src/scripts/generate-meta.ts
+++ src/scripts/generate-meta.ts
@@ -6,76 +6,11 @@ import * as toposort from "toposort";
 import * as builder from "xmlbuilder";
 import { ResourceMap } from "../types/ResouceMap";
 import { ResourceFile } from "../types/ResourceFile";
+import { PackageConfig } from "../types/PackageConfig";
 
 interface GenerateMetaOptions {
   fullPath: string;
-  config: any;
-}
-
-async function getResourceScriptsList(fullPath: string, type: "client" | "server", cache: boolean) {
-  const buildPath = path.resolve(`${fullPath}/build/`);
-  let filesPaths = await getFilesPaths(buildPath) as string[];
-
-  filesPaths.filter(filePath => path.extname(filePath) === ".lua");
-
-  const filesDependencies = await Promise.all(
-    filesPaths.map(filePath => fsPromises.readFile(filePath, "utf8")
-      .then((content: string) => ({
-        fileName: path.relative(fullPath, filePath),
-        dependencies: getMatches(content, /require\("(.*)"\)/g)
-          .map(m => path.normalize(`build/${m.split(".").join("/")}.lua`)),
-      }))
-    )
-  );
-
-  let withoutRelations = [];
-  const relations: [ string, string ][] = [];
-
-  for (const item of filesDependencies) {
-    if (item.dependencies.length === 0) {
-      withoutRelations.push(item.fileName);
-      continue;
-    }
-
-    for (const dependency of item.dependencies) {
-      relations.push([ item.fileName, dependency ]);
-    }
-  }
-
-  const topologicalSortedByRelations = toposort(relations).reverse();
-  withoutRelations = withoutRelations.filter(item => !topologicalSortedByRelations.includes(item));
-
-  return [ ...topologicalSortedByRelations, ...withoutRelations ].map((filePath: string) => ({
-    src: filePath,
-    cache,
-    type
-  }));
-}
-
-async function getResourceFilesList(fullPath: string, files: ResourceFile[]) {
-  const result = [];
-
-  for (const item of files) {
-    const filesPaths = await getFilesPaths(item.src);
-    result.push(...filesPaths.map((filePath: string) => ({
-        src: path.relative(fullPath, filePath),
-        download: item.download
-    })));
-  }
-  return result;
-}
-
-async function getResourceMapsList(fullPath: string, maps: ResourceMap[]) {
-  const result = [];
-
-  for (const item of maps) {
-    const filesPaths = await getFilesPaths(item.src);
-    result.push(...filesPaths.map((filePath: string) => ({
-      src: path.relative(fullPath, filePath),
-      dimension: item.dimension
-    })));
-  }
-  return result;
+  config: PackageConfig;
 }
 
 export async function generateMeta(options: GenerateMetaOptions) {
@@ -148,6 +83,72 @@ function getMatches(string: string, regex: RegExp, index: number = 1) {
   }
 
   return matches;
+}
+
+async function getResourceScriptsList(fullPath: string, type: "client" | "server", cache: boolean) {
+  const buildPath = path.resolve(`${fullPath}/build/`);
+  let filesPaths = await getFilesPaths(buildPath) as string[];
+
+  filesPaths.filter(filePath => path.extname(filePath) === ".lua");
+
+  const filesDependencies = await Promise.all(
+    filesPaths.map(filePath => fsPromises.readFile(filePath, "utf8")
+      .then((content: string) => ({
+        fileName: path.relative(fullPath, filePath),
+        dependencies: getMatches(content, /require\("(.*)"\)/g)
+          .map(m => path.normalize(`build/${m.split(".").join("/")}.lua`)),
+      }))
+    )
+  );
+
+  let withoutRelations = [];
+  const relations: [ string, string ][] = [];
+
+  for (const item of filesDependencies) {
+    if (item.dependencies.length === 0) {
+      withoutRelations.push(item.fileName);
+      continue;
+    }
+
+    for (const dependency of item.dependencies) {
+      relations.push([ item.fileName, dependency ]);
+    }
+  }
+
+  const topologicalSortedByRelations = toposort(relations).reverse();
+  withoutRelations = withoutRelations.filter(item => !topologicalSortedByRelations.includes(item));
+
+  return [ ...topologicalSortedByRelations, ...withoutRelations ].map((filePath: string) => ({
+    src: filePath,
+    cache,
+    type
+  }));
+}
+
+async function getResourceFilesList(fullPath: string, files: ResourceFile[]) {
+  const result = [];
+
+  for (const item of files) {
+    const filesPaths = await getFilesPaths(item.src);
+    result.push(...filesPaths.map((filePath: string) => ({
+        src: path.relative(fullPath, filePath),
+        download: item.download
+    })));
+  }
+  return result;
+}
+
+async function getResourceMapsList(fullPath: string, maps: ResourceMap[]) {
+  const result = [];
+
+  for (const item of maps) {
+    const filesPaths = await getFilesPaths(item.src);
+    result.push(...filesPaths.map((filePath: string) => ({
+      src: path.relative(fullPath, filePath),
+      dimension: item.dimension
+    })));
+  }
+  return result;
 }
 
 async function getFilesPaths(directoryOrFilePath: string): Promise<any> {
