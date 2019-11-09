e76d9fe90dd6745cfe2ff3d2ed6ecc2ae0bc433e
Timur Popov <timur2915@gmail.com>
2019-11-09 22:36:55 +0300

    Update build process

--- src/scripts/build.ts
+++ src/scripts/build.ts
@@ -1,16 +1,37 @@
 import * as rimraf from "rimraf";
 import * as path from "path";
-import { spawnSync } from "child_process";
 import { generateMeta } from "./generate-meta";
+import { PackageConfig } from "../types/PackageConfig";
 
 interface IBuildOptions {
   fullPath: string;
-  config: any;
+  config: PackageConfig;
 }
 
 export async function buildProject(options: IBuildOptions) {
+  let tstl: any;
+  try {
+    tstl = await import("typescript-to-lua");
+  } catch (e) {
+    throw new Error("Package typescript-to-lua isn't installed");
+  }
+
+  let ts: any;
+  try {
+    ts = await import("typescript");
+  } catch (e) {
+    throw new Error("Package typescript isn't installed");
+  }
+
   const {config, fullPath} = options;
   rimraf.sync(path.resolve(fullPath, "/build"));
-  spawnSync("tstl", ["-p", path.resolve(fullPath, "/tsconfig.json")], { encoding: "utf8" });
+
+  const configFileName = path.join(fullPath, "tsconfig.json");
+  const { emitResult, diagnostics } = tstl.transpileProject(configFileName);
+  emitResult.forEach(({ name, text }: { name: string, text: string }) => ts.sys.writeFile(name, text));
+
+  const reportDiagnostic = tstl.createDiagnosticReporter(true);
+  diagnostics.forEach(reportDiagnostic);
+
   await generateMeta({ fullPath, config });
 }
